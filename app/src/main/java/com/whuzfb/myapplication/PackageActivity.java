dc7a1e6f8c3b126491503c70dc41a96df91f7225
zfb <zfb132@gmail.com>
2017-03-01 17:52:41 +0800

    修改Package的注释代码

--- app/src/main/java/com/whuzfb/myapplication/PackageActivity.java
+++ app/src/main/java/com/whuzfb/myapplication/PackageActivity.java
@@ -201,6 +201,7 @@ public class PackageActivity extends Activity implements AbsListView.OnScrollLis
         // 这是曾经安装过但是卸载了的软件|MATCH_UNINSTALLED_PACKAGES
         packagelist = pm.getInstalledPackages(PackageManager.GET_ACTIVITIES|GET_PERMISSIONS | GET_RECEIVERS | GET_SERVICES);
 
+        //packagelist=pm.getPreferredPackages(PackageManager.GET_ACTIVITIES|GET_PERMISSIONS | GET_RECEIVERS | GET_SERVICES);
         //获得应用的banner对象Drawable类型
         //pm.getApplicationBanner(packagename);
         //获得应用的设置int
