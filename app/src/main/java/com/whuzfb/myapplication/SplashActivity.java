8761d7fdf7db487eb2cedb3c3470fb0de516c9ef
zfb <zfb132@gmail.com>
2017-03-05 11:56:51 +0800

    添加SplashActivity来遮挡白屏

---
+++ app/src/main/java/com/whuzfb/myapplication/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.whuzfb.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+/**
+ * Created by zfb15 on 2017/3/5.
+ */
+
+public class SplashActivity extends Activity {
+    private TextView tv_version;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashactivity);
+
+        /*
+        //Display the current version number
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo("com.zj.splashdemo", 0);
+            tv_version = (TextView) findViewById(R.id.tv_version);
+            tv_version.setText("软件版本： " + packageInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        */
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+
+    }
+}
